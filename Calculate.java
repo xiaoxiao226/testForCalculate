51ead43f435fcbe19730541be36b0a18feb35370
xiaoxiao226 <xiaoxiao_wsy@126.com>
2015-05-23 11:57:15 +0800

    Create Calculate.java

---
+++ Calculate.java
@@ -0,0 +1,25 @@
+package testForJunit;
+
+public class Calculate {
+
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+	
+	public int minus(int a, int b)
+	{
+		return a - b;
+	}
+	
+	public int multi(int a, int b)
+	{
+		return a * b;
+	}
+	
+	public int divide(int a, int b)
+	{
+		return a / b;
+	}
+	
+}
